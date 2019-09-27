23bc565c3ad1a008352f09bdace4f9a3fee4e20f
jonli <jonli@tencent.com>
2019-09-27 16:09:42 +0800

    update for mingw build

--- mingw-cmake-build.bat
+++ mingw-cmake-build.bat
@@ -1,6 +1,10 @@
-rem mingw-make-generate.bat
-cmake -S . -B ./build/mingw -G "MinGW Makefiles"
+
+rem .\mingw-cmake-generate.bat
+cmake -DGLM_TEST_ENABLE=OFF -DBUILD_SHARED_LIBS=OFF -S . -B ./build/mingw -G "MinGW Makefiles"
 
 cd ./build/mingw
 
 mingw32-make
+
+cd ../..
+
--- mingw-cmake-generate.bat
+++ mingw-cmake-generate.bat
@@ -1,1 +1,4 @@
-cmake -S . -B ./build/mingw -G "MinGW Makefiles"
+
+
+rem DBUILD_SHARED_LIBS is used for glfw static
+cmake -DGLM_TEST_ENABLE=OFF -DBUILD_SHARED_LIBS=OFF -S . -B ./build/mingw -G "MinGW Makefiles"
