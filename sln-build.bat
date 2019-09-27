f6ad2024f9159192fd3ba9911d3d1de721448c40
jonli <jonli@tencent.com>
2019-09-27 23:11:03 +0800

    update sln-build.bat

--- sln-build.bat
+++ sln-build.bat
@@ -6,6 +6,7 @@ rem cmake --build ./build/win64/thirdparty/glm-0.9.9.6
 rem cmake -S ./thirdparty/glfw-3.3 -B ./build/win64/thirdparty/glfw-3.3 -G "Visual Studio 15 2017 Win64" -DGLM_TEST_ENABLE=OFF
 rem cmake --build ./build/win64/thirdparty/glfw-3.3
 
-./sln-generate.bat
+rem ./sln-generate.bat
+cmake -DGLM_TEST_ENABLE=OFF -DBUILD_SHARED_LIBS=OFF -S . -B ./build/win64 -G "Visual Studio 15 2017 Win64"
 
 cmake --build ./build/win64
--- sln-generate.bat
+++
@@ -1,3 +0,0 @@
-
-rem DBUILD_SHARED_LIBS is used for glfw static
-cmake -DGLM_TEST_ENABLE=OFF -DBUILD_SHARED_LIBS=OFF -S . -B ./build/win64 -G "Visual Studio 15 2017 Win64"
