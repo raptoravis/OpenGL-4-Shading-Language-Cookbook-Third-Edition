98e29d5b373035d7685813539dfb4f89c4ee5839
jonli <jonli@tencent.com>
2019-09-27 21:08:13 +0800

    update mingw build

--- mingw-cmake-build.bat
+++ mingw-cmake-build.bat
@@ -2,9 +2,8 @@
 rem .\mingw-cmake-generate.bat
 cmake -DGLM_TEST_ENABLE=OFF -DBUILD_SHARED_LIBS=OFF -S . -B ./build/mingw -G "MinGW Makefiles"
 
-cd ./build/mingw
-
-mingw32-make
-
-cd ../..
+rem cd ./build/mingw
+rem mingw32-make
+rem cd ../..
+cmake --build ./build/mingw 
 
