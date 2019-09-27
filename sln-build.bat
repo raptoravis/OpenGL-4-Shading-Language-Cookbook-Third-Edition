9502a3fa587c7c5d3e504311cffa9ee5ce7289c6
jonli <jonli@tencent.com>
2019-09-27 15:58:00 +0800

    update to use glfw3 static

--- sln-build.bat
+++ sln-build.bat
@@ -4,7 +4,6 @@ rem cmake --build ./build/win64/thirdparty/glm-0.9.9.6
 rem cmake -S ./thirdparty/glfw-3.3 -B ./build/win64/thirdparty/glfw-3.3 -G "Visual Studio 15 2017 Win64" -DGLM_TEST_ENABLE=OFF
 rem cmake --build ./build/win64/thirdparty/glfw-3.3
 
-
 ./sln-generate.bat
 
 cmake --build ./build/win64
--- sln-generate.bat
+++ sln-generate.bat
@@ -1,1 +1,4 @@
-cmake -DGLM_TEST_ENABLE=OFF -S . -B ./build/win64 -G "Visual Studio 15 2017 Win64"
+
+rem DBUILD_SHARED_LIBS is used for glfw static
+
+cmake -DGLM_TEST_ENABLE=OFF -DBUILD_SHARED_LIBS=OFF -S . -B ./build/win64 -G "Visual Studio 15 2017 Win64"
